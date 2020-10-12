83b4f1cc2f743a8b31b748a9e51eb23a6bbbf5fc
Andrew Starr <astarr93@outlook.com>
2020-10-12 03:30:50 -0400

    Added switch for inquirer answers and first operator questions
    Added first layer question and routes based on what option was selected

--- index.js
+++ index.js
@@ -9,25 +9,30 @@ figlet("Contoso CMS\n", function (err, data) {
         console.log(err);
     }
     else {
-        console.log(data + "\nContoso  Corp. Content Managment System");
+        console.log(data + "\nContoso Corp. Content Managment System");
     };
+    connectSQL();
 });
 
-// Setup local SQL database connection
-const connectSQL = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "QiCpT@n#vD5w6U0LPDl0",
-    database: "company_db"
-});
+function connectSQL() {
 
-// Connect to SQL DB
-connectSQL.connect((err) => {
-    if (err) throw err;
-    console.log("\nConnected to MySQL database using thread: " + connectSQL.threadId)
-    init();
-});
+    // Setup local SQL database connection
+    const connectSQL = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "QiCpT@n#vD5w6U0LPDl0",
+        database: "company_db"
+    });
+
+    // Connect to SQL DB
+    connectSQL.connect((err) => {
+        if (err) throw err;
+        console.log(`\nConnected to Contoso Corp. MySQL database using thread: ${connectSQL.threadId}\n`);
+        init();
+    });
+
+};
 
 //  App Start
 function init() {
@@ -41,14 +46,48 @@ function init() {
     function getInfo() {
         inquirer.prompt(menuOperator).then(answers => {
             // check to see if we need to go back
-            if (answers.hasMore !== false) {
-                // function(answers);
-                getInfo();
-            }
-            else {
-                // function(answers);
-                // function(answers);
+            switch (true) {
+                case ((answers.menu === 'View corporate data') && (answers.menuGet === 'View all departments')):
+                    console.log(1);
+                    getAllDepartments();
+                    break;
+                case ((answers.menu === 'View corporate data') && (answers.menuGet === 'View all employees')):
+                    console.log(2);
+                    getAllEmployees();
+                    break;
+                case ((answers.menu === 'View corporate data') && (answers.menuGet === 'View all roles')):
+                    console.log(3);
+                    getAllRoles();
+                    break;
+                case ((answers.menu === 'Add corporate data') && (answers.menuAdd === 'Add new department')):
+                    console.log(4);
+                    addNewDepartment();
+                    break;
+                case ((answers.menu === 'Add corporate data') && (answers.menuAdd === 'Add new employee')):
+                    console.log(5);
+                    addNewEmployee();
+                    break;
+                case ((answers.menu === 'Add corporate data') && (answers.menuAdd === 'Add new role')):
+                    console.log(6);
+                    addNewRole();
+                    break;
+                case ((answers.menu === 'Update corporate data') && (answers.menuUpdate === 'Update employee manager')):
+                    console.log(7);
+                    UpdateEmployeeManager();
+                    break;
+                case ((answers.menu === 'Update corporate data') && (answers.menuUpdate === 'Update employee role')):
+                    console.log(8);
+                    UpdateEmployeeRole();
+                    break;
+                case ((answers.menu === 'Delete corporate data') && (answers.menuDelete === 'DELETE employee manager')):
+                    console.log(10);
+                    deleteEmployeeManager();
+                    break;
+                case ((answers.menu === 'Delete corporate data') && (answers.menuDelete === 'DELETE employee role')):
+                    console.log(11);
+                    deleteEmployeeRole();
+                    break;
             };
         });
     };
-} ''
+};
