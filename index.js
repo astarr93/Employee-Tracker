04d6c418d8ee5451a1f265d8f4c65d5526083bf3
Andrew Starr <astarr93@outlook.com>
2020-10-12 02:57:50 -0400

    Added SQL connection and updated module export for questions
    Updated index.js and questions.js to include an export object for multiple inquirer questions.
    
    Also added connection to MySQL database

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const figlet = require("figlet");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const { menuOperator } = require("./lib/questions");
 
 // ASCII Art to Start Program
-figlet("Employee Management System", function (err, data) {
+figlet("Contoso CMS\n", function (err, data) {
     if (err) {
         console.log(err);
     }
     else {
-        console.log(data);
+        console.log(data + "\nContoso  Corp. Content Managment System");
     };
-    // Run app
+});
+
+// Setup local SQL database connection
+const connectSQL = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "QiCpT@n#vD5w6U0LPDl0",
+    database: "company_db"
+});
+
+// Connect to SQL DB
+connectSQL.connect((err) => {
+    if (err) throw err;
+    console.log("\nConnected to MySQL database using thread: " + connectSQL.threadId)
     init();
 });
 
 //  App Start
 function init() {
 
+    // Bring in all inquirer prompt questions
     const questions = require('./lib/questions');
 
     // node inquirer package collects user response, generates output
     getInfo();
 
     function getInfo() {
-        inquirer.prompt(questions).then(answers => {
+        inquirer.prompt(menuOperator).then(answers => {
             // check to see if we need to go back
             if (answers.hasMore !== false) {
                 // function(answers);
